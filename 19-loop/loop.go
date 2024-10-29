99c9cfabb0430fdb79d3d80cddbaf96bfd6c6450
dev-davimarques <davimarques10.dm@icloud.com>
2024-10-29 11:01:44 -0300

    outra forma de usar o for

--- 19-loop/loop.go
+++ 19-loop/loop.go
@@ -5,7 +5,10 @@ import(
 )
 
 func main(){
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+	for i := range 10{
+		fmt.Println("range", i)
 	}
 }
