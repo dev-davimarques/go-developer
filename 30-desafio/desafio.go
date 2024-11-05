52f2aa8b8c6a5fa1bc4764a0116b9b8116c259b1
dev-davimarques <davimarques10.dm@icloud.com>
2024-11-05 10:52:38 -0300

    desafio dio

---
+++ 30-desafio/desafio.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 {
+			fmt.Println("Número divisível por 3: ",i)
+		}
+	}
+
+	// for i := range 100{
+	// 	if i % 3 == 0 {
+	// 		fmt.Println("Número divisível por 3: ",i)
+	// 	}
+	// }
+}
