bb4fe40bff47c64a6db2b308ad6a0f6a480e1fcd
dev-davimarques <davimarques10.dm@icloud.com>
2024-11-13 14:17:26 -0300

    att v.2 desafio2

--- 31-desafio/desafio2.go
+++ 31-desafio/desafio2.go
@@ -5,15 +5,26 @@ import(
 )
 
 func main(){
-	for i := 1; i <=100; i++ {
-		if i%3==0 {
-			fmt.Println(i," - PIN")
-		}
-		if i%5==0 {
-			fmt.Println(i," - PAN")
-		}
-		if i%3==0 && i%5==0 {
-			fmt.Println(i," - PIN e PAN")
+	// for i := 1; i <=100; i++ {
+	// 	if i%3==0 {
+	// 		fmt.Println(i," - PIN")
+	// 	}
+	// 	if i%5==0 {
+	// 		fmt.Println(i," - PAN")
+	// 	}
+	// 	if i%3==0 && i%5==0 {
+	// 		fmt.Println(i," - PIN e PAN")
+	// 	}
+	// }
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("PingPang")
+		} else if i%3 == 0 {
+			fmt.Println("Ping")
+		} else if i%5 == 0 {
+			fmt.Println("Pang")
+		} else {
+			fmt.Println(i)
 		}
 	}
 }
