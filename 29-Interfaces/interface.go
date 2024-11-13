69fc56d052e716370a5101756aeccb82f7541aae
dev-davimarques <davimarques10.dm@icloud.com>
2024-11-13 14:01:16 -0300

    desafios dio

---
+++ 29-Interfaces/interface.go
@@ -0,0 +1,18 @@
+package main
+
+// O que são interfaces?
+// São coleções de métodos
+
+import (
+	// "fmt"
+	// "math"
+)
+
+type geometria interface{
+	area() float64
+	perimetro() float64
+}
+
+func main(){
+
+}
