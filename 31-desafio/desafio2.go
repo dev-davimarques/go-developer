d6e1ff6ad5d3290fc340a4a2459abfe5ee7ee937
dev-davimarques <davimarques10.dm@icloud.com>
2024-11-13 14:13:32 -0300

    att desafio2

--- 31-desafio/desafio2.go
+++ 31-desafio/desafio2.go
@@ -6,12 +6,14 @@ import(
 
 func main(){
 	for i := 1; i <=100; i++ {
-		if i%3 == 0 {
-			// fmt.Printf("%g é múltiplo de 3 [PIN]\n",i)
-			fmt.Println(i," - é PIN [MÚLTIPLO DE 3]")
-		} else if i%5==0 {
-			// fmt.Printf("%g é múltiplo de 5 [PAN]\n",i)
-			fmt.Println(i, " - é PAN [MÚLTIPLO DE 5]")
+		if i%3==0 {
+			fmt.Println(i," - PIN")
+		}
+		if i%5==0 {
+			fmt.Println(i," - PAN")
+		}
+		if i%3==0 && i%5==0 {
+			fmt.Println(i," - PIN e PAN")
 		}
 	}
 }
