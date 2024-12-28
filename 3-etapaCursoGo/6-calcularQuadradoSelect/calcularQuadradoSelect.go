e53f5a5fcca616d5c80917fc2e513faed2143328
dev-davimarques <davimarques10.dm@icloud.com>
2024-12-28 10:59:10 -0300

    adding another channel to select

--- 3-etapaCursoGo/6-calcularQuadradoSelect/calcularQuadradoSelect.go
+++ 3-etapaCursoGo/6-calcularQuadradoSelect/calcularQuadradoSelect.go
@@ -16,6 +16,7 @@ func main() {
 
 	c3 := make(chan int) 
 	c4 := make(chan int)
+    c5 := make(chan int)
 
     go func() {
         time.Sleep(1 * time.Second)
@@ -25,13 +26,19 @@ func main() {
         time.Sleep(2 * time.Second)
         c4 <- calcularQuadrado(4)
     }()
+    go func(){
+        time.Sleep(3 * time.Second)
+        c5 <- calcularQuadrado(6)
+    }()
 
-    for i := 0; i < 2; i++ {
+    for i := 0; i < 3; i++ {
         select {
         case msg1 := <-c3:
             fmt.Println("Quadrado do 1º número: ", msg1)
         case msg2 := <-c4:
             fmt.Println("Quadrado do 2º número: ", msg2)
+        case msg3 := <-c5:
+            fmt.Println("Quadrado do 3º número: ",msg3)
         }
     }
 }
